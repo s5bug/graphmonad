3b4f64c7022cd9f194e3d1c1f117e1e1bf559ef4
Anthony Cerruti <soren@disroot.org>
2020-04-18 11:09:20 -0700

    Correct definition of RankedSet

--- build.sbt
+++ build.sbt
@@ -3,9 +3,11 @@ lazy val core = (project in file("core")).settings(
   name := "graphmonad",
   version := "0.1.0",
   scalaVersion := "2.13.1",
+  resolvers += Resolver.bintrayRepo("alexknvl", "maven"),
   libraryDependencies ++= Seq(
     "org.typelevel" %% "cats-core" % "2.1.1",
     "com.chuusai" %% "shapeless" % "2.3.3",
+    "com.alexknvl"  %%  "polymorphic" % "0.5.0",
   ),
   addCompilerPlugin("org.typelevel" %% "kind-projector" % "0.11.0" cross CrossVersion.full),
 )
--- core/src/main/scala/tf/bug/graphmonad/h/H.scala
+++ core/src/main/scala/tf/bug/graphmonad/h/H.scala
@@ -1,16 +1,62 @@
 package tf.bug.graphmonad.h
 
-import shapeless.{Nat, Sized}
+import cats.{Applicative, Functor, Monad}
+import shapeless._
 
-case class H[+A](
+case class HyperGraph[A, L](
   vertices: Set[A],
-  edges: RankedSet[A, HE],
+  edges: RankedSet[A, L]
 )
 
-case class HE[+A, R <: Nat](
-  label: String,
-  head: A,
-  tail: Sized[Vector[A], R]
-)
+case class HyperEdge[A, L, R <: Nat](
+  label: L,
+  arity: R,
+  incidence: Sized[Vector[A], R]
+) {
+  def mapLabel[M](f: L => M): HyperEdge[A, M, R] = HyperEdge(f(label), arity, incidence)
+}
 
-trait RankedSet[A, Edge[_, _ <: Nat]]
+trait Arity[F[_]] {
+  type R <: Nat
+
+  val first: F[R]
+  val second: R
+
+  override def equals(o: Any): Boolean = o match {
+    case oa: Arity[F] =>
+      second == oa.second && first == oa.first
+    case _ => false
+  }
+}
+
+object Arity {
+  def apply[F[_], A <: Nat](f: F[A], s: A): Arity[F] = new Arity[F] {
+    override type R = A
+    override val first: F[A] = f
+    override val second: A = s
+  }
+}
+
+case class RankedSet[A, L](values: Set[Arity[HyperEdge[A, L, *]]]) {
+  def mapLabel[M](f: L => M): RankedSet[A, M] = RankedSet(
+    values.map { arity =>
+      val first = arity.first
+      val second = arity.second
+      Arity(first.mapLabel(f), second)
+    }
+  )
+}
+
+case class H[A, L](
+  t: RankedSet[HyperGraph[A, L], L]
+) {
+
+
+
+}
+
+object H {
+
+
+
+}
